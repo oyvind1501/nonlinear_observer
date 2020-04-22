6fe4e4f70c648f13d9de45381b81f260c62c1362
oyvind1501 <oyvind1401@hotmail.com>
2020-04-22 13:51:28 +0200

    Add skeleton, package.XML and CmakeList files

---
+++ apps/test/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "nonlinear_observer.h"
+
+
+#include <Eigen/Dense>
+#include <cmath>
+#include <unsupported/Eigen/MatrixFunctions>
+#include <vector>
+
+using namespace Eigen;
+using namespace std;
+
+
+// Test common functions
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    Eigen::Vector2d hei{1,2};
+
+
+
+
+
+    std::cout<<hei<<std::endl;
+    return 0;
+}
+
+
+
